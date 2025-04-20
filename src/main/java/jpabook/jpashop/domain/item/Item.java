a1ee6c49e8cdcee30658d7db91dd81b228f22a91
master <ted3131@diquest.com>
2025-04-20 10:27:27 +0900

    commit

--- src/main/java/jpabook/jpashop/domain/item/Item.java
+++ src/main/java/jpabook/jpashop/domain/item/Item.java
@@ -2,6 +2,7 @@ package jpabook.jpashop.domain.item;
 
 import jakarta.persistence.*;
 import jpabook.jpashop.domain.Category;
+import jpabook.jpashop.exception.NotEnoughStockException;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -23,6 +24,20 @@ public abstract class Item {
     private int price;
     private int stockQuantity;
 
-    @ManyToMany (mappedBy = "items", fetch = FetchType.LAZY)
+    @ManyToMany(mappedBy = "items", fetch = FetchType.LAZY)
     private List<Category> categories = new ArrayList<>();
+
+    //재고수량을 늘려주는 로직
+    public void addStock(int quantity) {
+        this.stockQuantity += quantity;
+    }
+    //재고 수량 감소
+    public void removeStock(int quantity){
+        int restStock = this.stockQuantity - quantity;
+
+        if(restStock < 0){
+            throw new NotEnoughStockException("need more stock");
+        }
+        this.stockQuantity = restStock;
+    }
 }
