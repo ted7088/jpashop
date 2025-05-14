16b0ac2844ab91181ad511630ba148fe4d5f7e4b
master <ted3131@diquest.com>
2025-05-14 10:48:07 +0900

    commit

---
+++ src/main/java/jpabook/jpashop/domain/controller/HomeController.java
@@ -0,0 +1,17 @@
+package jpabook.jpashop.domain.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@Slf4j
+public class HomeController {
+
+    @RequestMapping("/")
+    public String home(){
+        return "home";
+    }
+
+
+}
