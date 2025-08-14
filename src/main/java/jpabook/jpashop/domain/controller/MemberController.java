e1639b7656c32e4fae4aa6d21cfd6e9681a0003b
master <ted3131@diquest.com>
2025-08-14 14:15:21 +0900

    commit

--- src/main/java/jpabook/jpashop/domain/controller/MemberController.java
+++ src/main/java/jpabook/jpashop/domain/controller/MemberController.java
@@ -11,6 +11,7 @@ import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
 
@@ -21,29 +22,35 @@ public class MemberController {
     private final MemberService memberService;
 
     @GetMapping("/members/new")
-    public String createForm(Model model){
-        model.addAttribute("memberForm",new MemberForm());
-
+    public String createForm(Model model) {
+        if(!model.containsAttribute("memberForm")) {
+            model.addAttribute("memberForm", new MemberForm());
+        }
         return "members/createMemberForm";
     }
 
-    @PostMapping(value = "/members/new")
-    public String create(@Valid MemberForm form, BindingResult result) {
+
+    @PostMapping("/members/new")
+    public String create(@Valid MemberForm form, BindingResult result, RedirectAttributes redirectAttributes) {
         if (result.hasErrors()) {
             return "members/createMemberForm";
         }
-        Address address =
-                new Address(form.getCity(), form.getStreet(),
-                        form.getZipcode());
-        Member member = new Member();
-        member.setName(form.getName());
-        member.setAddress(address);
 
-        memberService.join(member);
+        try {
+            Address address = new Address(form.getCity(), form.getStreet(), form.getZipcode());
+            Member member = new Member();
+            member.setName(form.getName());
+            member.setAddress(address);
+
+            memberService.join(member);
+
+        } catch (IllegalStateException e) {
+            redirectAttributes.addFlashAttribute("errorMessage", e.getMessage());
+            return "redirect:/members/new"; // GET 요청으로 페이지 이동
+        }
 
         return "redirect:/";
     }
-
     @GetMapping(value = "/members")
     public String list(Model model) {
         List<Member> members = memberService.findMembers();
